016bd9480b7338e11399c9f2518a8ba763f3a9de
Breno Amaral <contato.brenoamaral.s@gmail.com>
2024-08-04 11:35:39 -0300

    feat: File 'frontend'

--- backend/src/main/java/hackathon/backend/controllers/InstitutionController.java
+++ backend/src/main/java/hackathon/backend/controllers/InstitutionController.java
@@ -2,19 +2,20 @@ package hackathon.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import hackathon.backend.entitys.Institution;
-import hackathon.backend.services.InstitutionService;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import hackathon.backend.entitys.Institution;
+import hackathon.backend.services.InstitutionService;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/abrigo")
+@CrossOrigin(origins = "http://localhost:4200") // Habilita o CORS para o frontend
 public class InstitutionController {
+
     @Autowired
     private InstitutionService service;
 
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Hackathon KipperDev & Rocketseat - Conectando Abrigos
+> [!NOTE]\
+> Repositório voltado ao projeto desenvolvido para o Hackathon KipperDev & Rocketseat.
+
+## Sobre o Projeto
+O projeto idealizado pela equipe Byte Busters tem como objetivo primordial desenvolver um marketplace virtual de resiliência que conecte abrigos para permitir a comunicação, doação e recebimento de itens essenciais para enfrentar crises climáticas, como kits de emergência, alimentos não perecíveis, ferramentas de sobrevivência e produtos diversos. Além disso, o sistema deve permitir o monitoramento e controle do consumo dos itens, previsões de chegada e estimativas de duração dos estoques.
